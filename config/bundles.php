f73dea7252fad94e7158ad8c89bc30c24b50ce20
mario-ubuntu <mariopastorlanchares@gmail.com>
2024-08-13 20:10:15 +0200

    feat - new plugin for CMS https://docs.sylius.com/en/1.13/getting-started-with-sylius/plugin-installation.html

--- config/bundles.php
+++ config/bundles.php
@@ -60,4 +60,6 @@ return [
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Sylius\PayPalPlugin\SyliusPayPalPlugin::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    BitBag\SyliusCmsPlugin\BitBagSyliusCmsPlugin::class => ['all' => true],
 ];
